e138dd3d9c0dcaf152c7da2202c2ce8c48928ca2
Alejandro <alejoguevarafm@gmail.com>
2023-05-31 13:37:49 -0400

    perf: :hammer: Configuración de Graph + Fixed Frontend
    Se hizo la configuración de graphql, incluyendo la configuración para la conexion de la base de datos. Y por ultimo se hizo un arreglo en el frontend para corregir el error de refresh en consola y los warning

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,21 +2,25 @@ import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider } from 'next-themes'
 import { darkTheme, lightTheme } from '../themes'
+import { useSSR } from '@nextui-org/react'
 
 function App({ Component, pageProps }: AppProps) {
+  const { isBrowser } = useSSR()
   return (
-    <ThemeProvider
-      defaultTheme='system'
-      attribute='class'
-      value={{
-        light: lightTheme.className,
-        dark: darkTheme.className
-      }}
-    >
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
-    </ThemeProvider>
+    isBrowser && (
+      <ThemeProvider
+        defaultTheme='system'
+        attribute='class'
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className
+        }}
+      >
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </ThemeProvider>
+    )
   )
 }
 
