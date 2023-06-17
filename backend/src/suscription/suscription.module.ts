ff4931ec8986574ff5c3b7964d9ded0898f0b66c
Gianfranco <gianlanza10@gmail.com>
2023-06-17 12:43:39 -0400

    refactor: :art: Eliminado la dependencia circular entre Users y Suscription
    Se quitó el reporte en suscriptions y se removió su ResolveField de User

--- backend/src/suscription/suscription.module.ts
+++ backend/src/suscription/suscription.module.ts
@@ -4,15 +4,16 @@ import { SuscriptionService } from './suscription.service';
 import { SuscriptionResolver } from './suscription.resolver';
 import { Suscription } from './entities/suscription.entity';
 import { MembershipsModule } from 'src/memberships/memberships.module';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
   providers: [SuscriptionResolver, SuscriptionService],
   imports: [
     TypeOrmModule.forFeature( [Suscription] ),
     MembershipsModule,
-    forwardRef( () => UsersModule),
   ],
-  exports: [ SuscriptionService ]
+  exports: [
+    TypeOrmModule,
+    SuscriptionService
+  ]
 })
 export class SuscriptionModule {}
--- backend/src/suscription/suscription.service.ts
+++ backend/src/suscription/suscription.service.ts
@@ -6,37 +6,38 @@ import { CreateSuscriptionInput, UpdateSuscriptionInput } from './dto/inputs';
 import { User } from 'src/users/entities';
 import { MembershipsService } from '../memberships/memberships.service';
 import { TypeMemberships } from 'src/memberships/enums/type-memberships.enum';
+import { Membership } from 'src/memberships/entities/membership.entity';
 
 
 @Injectable()
 export class SuscriptionService {
 
   constructor(
+
     private readonly membershipsService : MembershipsService,
 
     @InjectRepository(Suscription)  
     private readonly suscriptionRepository: Repository<Suscription>,
 
-    
-    @InjectRepository(User)  
-    private readonly userRepository: Repository<User>,
   ){}
 
   async createSuscription(createSuscriptionInput: CreateSuscriptionInput, user : User): Promise<Suscription> {
+
     try { 
+    
       const { dateSuscription } = createSuscriptionInput;
       const dateEnd = new Date(dateSuscription);
       dateEnd.setMonth(dateEnd.getMonth() + 1);
       
       const membership = await this.membershipsService.findOne(createSuscriptionInput.membership);
+
       const newSuscription = this.suscriptionRepository.create({
-        membership : membership.id,
-        user : user.id,
         dateSuscription: new Date(dateSuscription),
-        dateEnd, 
+        dateEnd : dateEnd,
         userId : user.id,
         membershipId : membership.id,
       })
+      
       return await this.suscriptionRepository.save(newSuscription);
     } catch (error) {
       console.log(error)
@@ -63,49 +64,10 @@ export class SuscriptionService {
     }
   }
 
-  async reportSuscriptions() : Promise<Suscription[]> {
-    try {
-      
-      const { id  :  goldId } = await this.membershipsService.findOneByName(TypeMemberships.Gold);  
-      const { id  :  premiumId } = await this.membershipsService.findOneByName(TypeMemberships.Premium);  
-      const today = new Date()
-      const fourMonthsAgo = new Date()
-      fourMonthsAgo.setMonth(fourMonthsAgo.getMonth() - 4);
 
-      const queryresult = await this.suscriptionRepository.query(`
-        SELECT *
-        from "Suscription"
-        where "membershipId" = '${premiumId}'
-          AND "isActive" = TRUE
-          AND "userId" IN ( SELECT "userId"
-                            from "Suscription"
-                            where "membershipId" = '${goldId}'
-                            AND "isActive" = false
-                            AND "dateEnd" BETWEEN '${fourMonthsAgo.toISOString().slice(0,10)}' AND '${today.toISOString().slice(0,10)}')
-      `)
-      return queryresult
-
-    } catch (error) {
-      console.log(error)
-      throw new InternalServerErrorException('Error en el servidor')
-    }
-
-  }
-
-  async findMembershipByUser(id: string): Promise<any[]>{
-    const suscriptionActive = await this.suscriptionRepository.findOne({
-      where:{
-        userId: id,
-        isActive: true,
-      }
-    })
-    return await this.membershipsService.findOtherMembership(suscriptionActive.membershipId);
-  }
-
-  async findUserById(userId : string) : Promise<User>{
-    return await this.userRepository.findOneBy({
-        id : userId
-      })
+  async findMembershipByUser(user : User): Promise<Membership>{
+    const suscriptionActive = await this.findOneActiveByUser(user)
+    return await this.membershipsService.findOne(suscriptionActive.membershipId)
   }
 
   update(id: number, updateSuscriptionInput: UpdateSuscriptionInput) {
