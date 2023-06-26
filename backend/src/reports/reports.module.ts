e211cdc48fd3c95cf24a10338d3d15eae7a6094b
Gianfranco <gianlanza10@gmail.com>
2023-06-25 23:05:42 -0400

    feat: :sparkles: Report Serie and Movie

--- backend/src/reports/reports.module.ts
+++ backend/src/reports/reports.module.ts
@@ -3,11 +3,13 @@ import { ReportsResolver } from './reports.resolver';
 import { UsersModule } from 'src/users/users.module';
 import { MembershipsModule } from 'src/memberships/memberships.module';
 import { SuscriptionModule } from 'src/suscription/suscription.module';
+import { MediaModule } from 'src/media/media.module';
 
 @Module({
   providers: [ ReportsResolver ],
   imports : [
     SuscriptionModule,
+    MediaModule
   ]
 
 })
--- backend/src/reports/reports.resolver.ts
+++ backend/src/reports/reports.resolver.ts
@@ -1,17 +1,32 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { Suscription } from 'src/suscription/entities/suscription.entity';
 import { SuscriptionService } from '../suscription/suscription.service';
+import { Movie, Serie } from 'src/media/entities';
+import { MediaService } from '../media/media.service';
 
 @Resolver()
 export class ReportsResolver {
 
     constructor (
-        private readonly suscriptionService : SuscriptionService
+        private readonly suscriptionService : SuscriptionService,
+
+        private readonly mediaService : MediaService
     ) {}
 
     @Query(() => [Suscription], { name : 'reportSuscription'})
     async reportSuscription() : Promise<Suscription[]>{
         return this.suscriptionService.reportSuscriptions()
+    }
+
+
+    @Query(() => [Serie], { name : 'reportSerie' })
+    async reportSerie() : Promise<Serie[]>{
+        return this.mediaService.reportSerie()
+    }
+    
+    @Query(() => [Movie], { name : 'reportMovie' })
+    async reportMovie() : Promise<Movie[]>{
+        return this.mediaService.reportMovie()
     }
 
 }
